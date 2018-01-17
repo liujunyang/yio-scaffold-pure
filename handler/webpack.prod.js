c615d1851f2191cb4ec8f44f4076759272571672
liujunyang <liujunyangscript@gmail.com>
2018-01-17 20:59:34 +0800

    add handlers

---
+++ handler/webpack.prod.js
@@ -0,0 +1,69 @@
+module.exports = (context) => {
+    const webpack = require('webpack');
+    const merge = require('webpack-merge');
+
+    const ExtractTextPlugin = require('extract-text-webpack-plugin');
+    const PluginPresetHtml = require('./plugin-preset-html');
+
+    // reset context
+    const userConfig = require('./util-get-user-config')({ ...context, webpack });
+
+    const { fastConfig } = userConfig;
+    const { replace } = fastConfig;
+
+    Object.assign(context, userConfig.context);
+
+    const { srcDir, distDir, taskName } = context;
+
+    const common = require('./webpack.common')({ context, fastConfig });
+
+    const finalWebpackConfig = merge(common, {
+        plugins: [
+            new PluginPresetHtml({
+                srcDir,
+                distDir,
+                watch: false,
+                compress: true,
+                replace,
+                taskName,
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: true,
+                },
+            }),
+            new ExtractTextPlugin({
+                filename: '[name].css',
+                disable: false,
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: true,
+                },
+            }),
+        ],
+    });
+
+    finalWebpackConfig.module.rules.unshift({
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader?minimize', 'postcss-loader'],
+                fallback: 'style-loader',
+            }),
+        }, {
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader?minimize', 'postcss-loader', 'less-loader'],
+                fallback: 'style-loader',
+            }),
+        });
+
+    console.log('Compiling...');
+    webpack(finalWebpackConfig, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Compilication done.');
+        }
+    });
+};
