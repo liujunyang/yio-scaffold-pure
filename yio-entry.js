2edadbbe3bc17d8e654ca47219e8b3e54c3c749c
liujunyang <liujunyangscript@gmail.com>
2018-01-26 19:10:32 +0800

    test ls

--- yio-entry.js
+++ yio-entry.js
@@ -1,3 +1,19 @@
+const { spawn } = require('child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+return;
+
 var path = require('path');
 var fs = require('fs');
 
