ff5ab96417d997a61c589edfd636f3e1c4a1719b
liujunyang <liujunyangscript@gmail.com>
2018-01-27 16:40:33 +0800

    local through

--- yio-entry.js
+++ yio-entry.js
@@ -1,17 +1,10 @@
-const { spawn } = require('child_process');
-const ls = spawn('ls', ['-lh', '/usr']);
-
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+const { execSync } = require('child_process');
+const ls = execSync('npm run dev', {
+	stdio: 'inherit',
+	cwd: '/Users/liujunyang/henry/work/yio-scaffold-pure'
 });
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+console.log('to be continued...')
 return;
 
 var path = require('path');
